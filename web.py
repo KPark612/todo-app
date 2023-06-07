c1c037649ef9019632a1c10be9c6b06f69e87bf0
Kpark <noah.jeffrey.hansen@gmail.com>
2023-06-07 17:11:47 -0500

    Implement "Complete todo items" feature

--- web.py
+++ web.py
@@ -17,11 +17,14 @@ st.subheader("This is my todo app.")
 st.write("This app is to increase your productivity.")
 
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.experimental_rerun()
 
 st.text_input(label="Enter a todo", label_visibility='hidden',
               placeholder="Enter a new todo...",
               on_change=add_todo, key='new_todo')
-
-print('hello')
